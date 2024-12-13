82684a238bcd45de3bc2eb4be810de413b1f27ab
Ali <aliaynechiyan@gmail.com>
2024-12-13 21:21:52 +0330

    Update ProgressWheel widget to use ListWheelScrollView for improved performance and responsiveness. Add infinite_carousel dependency in pubspec.yaml and pubspec.lock for enhanced UI functionality.

--- lib/presentation/home/widget/crescent_clipper.dart
+++ lib/presentation/home/widget/crescent_clipper.dart
@@ -4,26 +4,23 @@ class CrescentClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
-    
-    // Starting point (شروع از نقطه پایین‌تر)
+
     path.moveTo(0, 60);
-    
-    // Create the curved top with higher control point
+
     path.quadraticBezierTo(
-      size.width / 2, // control point x
-      -40, // control point y (نقطه کنترل بالاتر برای قوس بیشتر)
-      size.width, // end point x
-      60, // end point y (پایان در همان ارتفاع شروع)
+      size.width / 2,
+      -40,
+      size.width,
+      60,
     );
-    
-    // Complete the rectangle
+
     path.lineTo(size.width, size.height);
     path.lineTo(0, size.height);
     path.close();
-    
+
     return path;
   }
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
-} 
+}
--- lib/presentation/home/widget/progress_wheel.dart
+++ lib/presentation/home/widget/progress_wheel.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'dart:math' as math;
 
 class SubjectItem {
   final String name;
@@ -35,45 +34,67 @@ class ProgressWheel extends StatefulWidget {
 }
 
 class _ProgressWheelState extends State<ProgressWheel> {
-  late PageController _pageController;
+  late FixedExtentScrollController _controller;
   int _selectedIndex = 0;
-  final viewportFraction = 0.3; // Adjusted for better spacing
 
   @override
   void initState() {
     super.initState();
-    _pageController = PageController(viewportFraction: viewportFraction);
+    _controller = FixedExtentScrollController(
+      initialItem: widget.subjects.length * 100 + (widget.subjects.length ~/ 2),
+    );
+    _selectedIndex = widget.subjects.length ~/ 2;
   }
 
   @override
   void dispose() {
-    _pageController.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final repeatedSubjects = <SubjectItem>[];
-    while (repeatedSubjects.length < 15) {
-      repeatedSubjects.addAll(widget.subjects);
-    }
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final screenWidth = constraints.maxWidth;
+        final itemWidth = screenWidth * 0.2;
 
-    return SizedBox(
-      height: widget.height,
-      child: MyPageView(
-        children: repeatedSubjects.map((subject) {
-          final isSelected = repeatedSubjects.indexOf(subject) % widget.subjects.length == _selectedIndex;
-          return GestureDetector(
-            onTap: () {
-              setState(() {
-                _selectedIndex = repeatedSubjects.indexOf(subject) % widget.subjects.length;
-              });
-              widget.onTap?.call();
-            },
-            child: _buildSubjectWidget(subject, isSelected),
-          );
-        }).toList(),
-      ),
+        return SizedBox(
+          height: widget.height,
+          child: RotatedBox(
+            quarterTurns: 3, // Rotate the list to make it horizontal
+            child: ListWheelScrollView.useDelegate(
+              controller: _controller,
+              itemExtent: itemWidth,
+              perspective: 0.0015, // Reduced perspective for gentler curve
+              diameterRatio: 4, // Increased for flatter curve
+              physics: const FixedExtentScrollPhysics(),
+              onSelectedItemChanged: (index) {
+                setState(() {
+                  _selectedIndex = index % widget.subjects.length;
+                });
+                widget.onTap?.call();
+              },
+              childDelegate: ListWheelChildLoopingListDelegate(
+                children: List.generate(widget.subjects.length * 1000, (index) {
+                  final realIndex = index % widget.subjects.length;
+                  final subject = widget.subjects[realIndex];
+                  final isSelected = realIndex == _selectedIndex;
+
+                  return RotatedBox(
+                    quarterTurns: 1, // Rotate items back to normal
+                    child: AnimatedOpacity(
+                      duration: const Duration(milliseconds: 200),
+                      opacity: isSelected ? 1.0 : 0.5,
+                      child: _buildSubjectWidget(subject, isSelected),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 
@@ -131,84 +152,5 @@ class _ProgressWheelState extends State<ProgressWheel> {
         ),
       ],
     );
-  }
-}
-
-const viewportFraction = 0.3;
-
-class MyPageView extends StatefulWidget {
-  const MyPageView({
-    required this.children,
-    Key? key,
-  }) : super(key: key);
-
-  final List<Widget> children;
-
-  factory MyPageView.images(List<ImageProvider> imageProviders) {
-    return MyPageView(
-      children: [
-        for (final image in imageProviders)
-          ClipRRect(
-            borderRadius: BorderRadius.circular(16),
-            child: Image(
-              image: image,
-              fit: BoxFit.cover,
-            ),
-          ),
-      ],
-    );
-  }
-
-  @override
-  State<MyPageView> createState() => _MyPageViewState();
-}
-
-class _MyPageViewState extends State<MyPageView> {
-  final pageController = PageController(viewportFraction: 0.2);
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraint) {
-        final maxWidth = constraint.maxWidth;
-        return PageView.builder(
-          allowImplicitScrolling: true,
-          controller: pageController,
-          itemCount: widget.children.length,
-          itemBuilder: (_, index) {
-            final child = widget.children[index % widget.children.length];
-            return AnimatedBuilder(
-              animation: pageController,
-              builder: (_, __) {
-                final ratioX =
-                    pageController.offset / maxWidth / 0.2 - index;
-
-                final angle = math.pi * -0.03 * ratioX;
-                final scale = 1.0 - ratioX.abs() * 0.3;
-                final translateX = ratioX * 8;
-                final translateY = ratioX.abs() * 50;
-
-                return Transform.rotate(
-                  angle: angle,
-                  child: Transform.translate(
-                    offset: Offset(translateX, translateY),
-                    child: Transform.scale(
-                      scale: scale,
-                      child: child,
-                    ),
-                  ),
-                );
-              },
-            );
-          },
-        );
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    pageController.dispose();
-    super.dispose();
   }
 }
